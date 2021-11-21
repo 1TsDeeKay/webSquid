8fb4894f429a84ca86b8b4488b652d6890bd5361
Dennis Kretz <94295986+1TsDeeKay@users.noreply.github.com>
2021-11-21 18:20:41 +0100

    alpha release

--- dirChecker/dirCheckerModule.py
+++ dirChecker/dirCheckerModule.py
@@ -1,4 +1,5 @@
-import requests 
+import requests
+from colorama import Fore, Back, Style
 
 
 
@@ -30,16 +31,33 @@ def secondarySetup():
 
 def launchDirChecker():
 
-    a_file = open(wordlist + ".txt")
+    with open(wordlist + ".txt") as wordlisttxt:
+        data = wordlisttxt.read()
 
-    lines = a_file.readlines()
+        lines = data.split("\n")
     for getWordlist in lines:
         r = requests.get(url + getWordlist)
 
         if (r.status_code == 200):
-            print(r.url, "<-- URL FOUND", r.status_code)
-        else:
-            print(r.url, "<-- URL NOT FOUND", r.status_code)
+            print(Fore.GREEN + "[+]", r.url, "<-- URL FOUND", r.status_code)
+        if (url == r.url):
+            print(Fore.YELLOW + "[!]", r.url, "<-- INDEX REDIRECT DETECTED :", url,getWordlist)
+        elif (getWordlist in r.url and r.status_code != 200 and "?redirect" in r.url):
+            print(Fore.GREEN + "[+]", r.url, "<-- URL REDIRECT DETECTED :", url,getWordlist)
+        if (r.status_code == 400):
+            print(Fore.RED + "[-]", r.url, "<-- BAD REQUEST ", r.status_code)
+        elif (r.status_code == 401):
+            print(Fore.RED + "[-]", r.url, "<-- UNAUTHORIZED ", r.status_code)
+        elif (r.status_code == 401):
+            print(Fore.RED + "[-]", r.url, "<-- FORBIDDEN ", r.status_code)
+        elif (r.status_code == 404):
+            print(Fore.RED + "[-]", r.url, "<-- URL NOT FOUND ", r.status_code)
+        elif (r.status_code == 500):
+            print(Fore.RED + "[-]", r.url, "<-- INTERNAL SERVER ERROR", r.status_code)
+        elif (r.status_code == 503):
+            print(Fore.RED + "[-]", r.url, "<-- SERVICE UNAVAILABLE ", r.status_code)
+
+
 
 
 mainSetup()
